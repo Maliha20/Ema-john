2e079dba768a9ff78f6cf318ffce02065ecaac55
maliha20 <gulista.marium20@gmail.com>
2023-03-28 23:37:09 +0600

    order summary added

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.product);
     const {name,img,seller,quantity, price, ratings} = props.product;
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +15,9 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating: {ratings} stars</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={()=> handleAddToCart(props.product)} className='btn-cart'>
+                Add to cart
+                 <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
