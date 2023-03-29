bcc574ad50a53c0006368561258112820065a98f
maliha20 <gulista.marium20@gmail.com>
2023-03-29 12:36:23 +0600

    saved cart into localstorage

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -6,15 +7,22 @@ import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    
     useEffect( ()=>{
         fetch('products.json')
         .then(res=> res.json())
         .then(data =>setProducts(data));
     },[]);
 
+    useEffect(()=>{
+       const storedCart = getShoppingCart();
+       console.log(storedCart)
+    },[])
+
     const handleAddToCart = (product)=>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
     return (
         <div className='shop-container'>
@@ -24,6 +32,7 @@ const Shop = () => {
                 product={product}
                 handleAddToCart = {handleAddToCart}
                 ></Product>)}
+            
             </div>
             <div className='cart-container'>
                    <Cart cart={cart}></Cart>
