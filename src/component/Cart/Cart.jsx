5a53f66c4aced8e0db8eb45e8465c7f752a052bb
maliha20 <gulista.marium20@gmail.com>
2023-03-29 13:31:46 +0600

    quanitity fixed

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -4,10 +4,15 @@ import './Cart.css'
 const Cart = ({cart}) => {
     console.log(cart)
     let total = 0;
+    let quantity = 0;
     let totalShipping = 0;
     for(const product of cart){
-        total = total + product.price;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity =product.quantity;
     }
     const tax = total*7/100;
     const grandTotal = total + totalShipping + tax;
